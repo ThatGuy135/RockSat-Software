c290c43635f012bc848f2ef7829cff78f713cdf3
Aidan <shaferaidan@gmail.com>
2026-02-08 14:55:00 -0800

    Update main.ino

--- TS/src/main.ino
+++ TS/src/main.ino
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include "Thermocouple_I2C_Control.h"
 #include <DualSD.h>
+#include <ThreeWayLogger.h>
 
 DualSD DualSDManager;
 
 void setup(){
 
   Serial.begin(115200);
+  LOGGER.begin(&DualSDManager);
 
   while (!Serial){
     delay(10);
@@ -24,4 +26,7 @@ void loop(){
   delay(1000);
 }
 
+void createCSVHeader(){
+  DualSDManager.initializeFiles("seconds_after_power,hot_junction_1,hot_junction_2,hot_junction_3,hot_junction_4,hot_junction_5,cold_junction_1,cold_junction_2,cold_junction_3,cold_junction_4,cold_junction_5,ADC_1,ADC_2,ADC_3,ADC_4,ADC_5,timed_event_detected_bool");
+}
 
